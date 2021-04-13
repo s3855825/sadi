cfdd83528aabe764dad4093bc646bb53cea2534b
s3855825 <s3855825@rmit.edu.vn>
2021-04-13 10:44:43 +0700

    implements StudentEnrolment methods

--- assignment/a1/StudentEnrolment.java
+++ assignment/a1/StudentEnrolment.java
@@ -1,9 +1,13 @@
 package assignment.a1;
 
+import java.util.ArrayList;
+
 public class StudentEnrolment implements StudentEnrolmentManager {
     private Student student;
     private Course course;
     private String semester;
+
+    private static ArrayList<StudentEnrolment> studentEnrolments = new ArrayList<StudentEnrolment>();
 
     public StudentEnrolment() {
     }
@@ -14,79 +18,51 @@ public class StudentEnrolment implements StudentEnrolmentManager {
         semester = newSemester;
     }
 
-    public Student getStudent() {
-        return student;
-    }
-
-    public Course getCourse() {
-        return course;
-    }
-
-    public String getSemester() {
-        return semester;
-    }
-
-    public void setStudent(Student newStudent) {
-        student = newStudent;
-    }
-
-    public void setCourse(Course newCourse) {
-        course = newCourse;
-    }
-
-    public void setSemester(String newSemester) {
-        semester = newSemester;
-    }
-
-    @Override
-    public void add(StudentEnrolment newStudentEnrolment) {
-        // TODO Auto-generated method stub
-    }
-
     @Override
     public void add(Student newStudent, Course newCourse, String newSemester) {
         // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void delete(StudentEnrolment StudentEnrolment) {
-        // TODO Auto-generated method stub
-
+        StudentEnrolment newStudentEnrolment = new StudentEnrolment(newStudent, newCourse, newSemester);
+        studentEnrolments.add(newStudentEnrolment);
     }
 
     @Override
     public void delete(Student student, Course course, String semester) {
         // TODO Auto-generated method stub
-
+        StudentEnrolment newStudentEnrolment = new StudentEnrolment(student, course, semester);
+        studentEnrolments.remove(newStudentEnrolment);
     }
 
     @Override
-    public void update(StudentEnrolment studentEnrolment, Student newStudent) {
+    public void update(StudentEnrolment studentEnrolment, Student newStudent, Course newCourse, String newSemester) {
         // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void update(StudentEnrolment studentEnrolment, Course newCourse) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void update(StudentEnrolment studentEnrolment, StudentEnrolment newStudentEnrolment) {
-        // TODO Auto-generated method stub
-
+        StudentEnrolment newStudentEnrolment = new StudentEnrolment(newStudent, newCourse, newSemester);
+        for (int i = 0; i < studentEnrolments.size(); i++) {
+            if (studentEnrolments.get(i) == newStudentEnrolment) {
+                studentEnrolments.set(i, newStudentEnrolment);
+            }
+        }
     }
 
     @Override
     public StudentEnrolment getOne(String studentId, String courseId, String semester) {
         // TODO Auto-generated method stub
-        return null;
+        try {
+            for (int i = 0; i < studentEnrolments.size(); i++) {
+                StudentEnrolment enrolmentRecord = studentEnrolments.get(i);
+                if (enrolmentRecord.student.getId() == studentId && enrolmentRecord.course.getId() == courseId
+                        && enrolmentRecord.semester.equals(semester)) {
+                    return enrolmentRecord;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RecordNotFoundException();
+        }
     }
 
     @Override
-    public StudentEnrolment getAll() {
+    public ArrayList<StudentEnrolment> getAll() {
         // TODO Auto-generated method stub
-        return null;
+        return studentEnrolments;
     }
 }
