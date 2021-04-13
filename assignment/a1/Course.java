e5126b8d21b4f321a32cee784767f31a41fca11c
s3855825 <s3855825@rmit.edu.vn>
2021-04-13 16:36:43 +0700

    update Course constructor

--- assignment/a1/Course.java
+++ assignment/a1/Course.java
@@ -1,17 +1,30 @@
 package assignment.a1;
 
+import java.util.Scanner;
+
 public class Course {
     private String id;
     private String name;
     private int numCredit;
 
     public Course() {
-    }
+        InputValidator inputValidator = new InputValidator();
+        Scanner scanner = new Scanner(System.in);
 
-    public Course(String newId, String newName, int newNumCredit) {
-        id = newId;
-        name = newName;
-        numCredit = newNumCredit;
+        System.out.println("Enter course name: ");
+        name = scanner.nextLine();
+
+        System.out.println("Enter course id: ");
+        id = scanner.nextLine();
+        while (!inputValidator.isValidPattern(id, 3)) {
+            System.out.println("Invalid id pattern. Enter again.");
+            id = scanner.nextLine();
+        }
+
+        System.out.println("Enter number of credit: ");
+        numCredit = scanner.nextInt();
+
+        scanner.close();
     }
 
     public String getId() {
--- assignment/a1/InputValidator.java
+++ assignment/a1/InputValidator.java
@@ -3,19 +3,24 @@ package assignment.a1;
 import java.util.regex.Pattern;
 
 public class InputValidator {
-    private String idPattern = "[s]+\\d{7}";
+    private String studentIdPattern = "[s]+\\d{7}";
     private String semesterPattern = "[2]\\d{3}[ABC]";
     private String dateFormat = "(0[1-9]|1[012])/(0[1-9]|[12][0-9]|3[01])/(19|20)[0-9][0-9]";
 
+    private String courseIdPattern = "[A-Z]{3,4}\\d{4}";
+
     public boolean isValidPattern(String input, int type) {
         if (type == 0) {
-            Pattern matchPattern = Pattern.compile(idPattern);
+            Pattern matchPattern = Pattern.compile(studentIdPattern);
             return matchPattern.matcher(input).matches();
         } else if (type == 1) {
             Pattern matchPattern = Pattern.compile(semesterPattern);
             return matchPattern.matcher(input).matches();
-        } else {
+        } else if (type == 2) {
             Pattern matchPattern = Pattern.compile(dateFormat);
+            return matchPattern.matcher(input).matches();
+        } else {
+            Pattern matchPattern = Pattern.compile(courseIdPattern);
             return matchPattern.matcher(input).matches();
         }
     }
