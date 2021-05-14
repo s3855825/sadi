a38fab1930800fbe882875e81c241fd5fef160e1
s3855825 <s3855825@rmit.edu.vn>
2021-05-14 15:41:15 +0700

    init spring project a2

---
+++ assignment/a2/demo/src/main/java/com/example/demo/Sedia2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sedia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sedia2Application.class, args);
+	}
+
+}
